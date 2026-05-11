b396f195bed27ab012829eddccbaba18e1e8510e
Aishani-Roy <aishaniroy2202@gmail.com>
2026-05-11 16:13:03 +0530

    py

---
+++ 1 to n.py
@@ -0,0 +1,6 @@
+n=int(input("enter number:"))
+i=1
+while i<n+1:
+    print(i)
+    i+=1
+
---
+++ sum.py
@@ -0,0 +1,7 @@
+n=int(input("enter number:"))
+i=1
+sum=0
+while i<n+1:
+    sum=sum+i
+    print(i,sum)
+    i+=1
